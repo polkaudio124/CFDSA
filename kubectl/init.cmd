511cbe1a09202cf0ecf3d90e56a6d78744c7ff7e
ngch <ngch@globalpsa.com>
2023-04-11 10:46:20 +0800

    day02 - updates

---
+++ kubectl/init.cmd
@@ -0,0 +1,5 @@
+@echo off
+
+set PATH="D:\OneDrive - PSA International\Course -  Containers for Deploying and Scaling Apps\CFDSA\kubectl\kubectl.exe";"D:\tmp\jdk\jdk-11.0.18+10\bin";"C:\gradle\gradle-6.8.3";%PATH%
+
+ENDLOCAL
